7e8519b465cfffa3d4d79edf499f58e6558a3aed
Shakir Shakiel <shakirshakiel@gmail.com>
2019-02-21 16:55:30 +0530

    Add support for output

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "sample_output" {
+    description = "A sample output"
+    value = "127.0.0.1"
+}
--- wiki_generator.rb
+++ wiki_generator.rb
@@ -22,37 +22,73 @@ def get_default(hash)
     hash["default"]
 end
 
-def print_wiki(hash)
+def print_input_wiki_line(hash)
     print "| #{hash['name']} | #{hash['description']} | #{hash['type']} | #{hash['default']} | #{hash['required']} | \n"
 end
 
-hcl = File.read(ARGV[0])
-parsed = HCL::Checker.parse(hcl)["variable"]
+def print_input_wiki(content)
+    input_parsed = HCL::Checker.parse(content)["variable"]
+    required_arr = []
+    non_required_arr = []
+    
+    input_parsed.each do |k, v|
+        name = k
+        description = v["description"]
+        type = get_type(v)
+        default = get_default(v)
+        required = default ? "No" : "Yes"
+    
+        h = {
+            'name' => name,
+            'description' => description,
+            'type' => type,
+            'default' => default,
+            'required' => required
+        }
+    
+        default ? non_required_arr.push(h) : required_arr.push(h)
+    end
 
-required_arr = []
-non_required_arr = []
-
-parsed.each do |k, v|
-    name = k
-    description = v["description"]
-    type = get_type(v)
-    default = get_default(v)
-    required = default ? "No" : "Yes"
-
-    h = {
-        'name' => name,
-        'description' => description,
-        'type' => type,
-        'default' => default,
-        'required' => required
-    }
-
-    default ? non_required_arr.push(h) : required_arr.push(h)
+    print "## Inputs\n\n"
+    print_input_wiki_line({'name' => 'Name', 'description' => 'Description', 'type' => 'Type', 'default' => 'Default', 'required' => 'Required'})
+    print_input_wiki_line({'name' => '------', 'description' => '------', 'type' => '------', 'default' => '------', 'required' => '------'})
+    required_arr.map {|h| print_input_wiki_line(h)}
+    non_required_arr.map {|h| print_input_wiki_line(h)}
+    print "\n"            
 end
 
-print_wiki({'name' => 'Name', 'description' => 'Description', 'type' => 'Type', 'default' => 'Default', 'required' => 'Required'})
-print_wiki({'name' => '------', 'description' => '------', 'type' => '------', 'default' => '------', 'required' => '------'})
-required_arr.map {|h| print_wiki(h)}
-non_required_arr.map {|h| print_wiki(h)}
+def print_output_wiki_line(hash)
+    print "| #{hash['name']} | #{hash['description']} | \n"
+end
+
+def print_output_wiki(content)
+    output_parsed = HCL::Checker.parse(content)["output"]
+
+    output_arr = []
+    output_parsed.each do |k, v|
+        name = k    
+        description = v["description"]
+        h = {
+            'name' => name,
+            'description' => description
+        }
+        output_arr.push(h)
+    end
+    
+    print "## Outputs\n\n"
+    print_output_wiki_line({'name' => 'Name', 'description' => 'Description'})
+    print_output_wiki_line({'name' => '------', 'description' => '------'})
+    output_arr.map {|h| print_output_wiki_line(h) }
+    print "\n"    
+end
+
+input_hcl = File.read(ARGV[0])
+print_input_wiki(input_hcl)
+
+if ARGV[1]
+    output_hcl = File.read(ARGV[1])
+    print_output_wiki(output_hcl)
+end
+
 
 
