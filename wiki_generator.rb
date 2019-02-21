f53e5a2eafaf77ef3fd3136d0a98ea422c7635b2
Shakir Shakiel <shakirshakiel@gmail.com>
2019-02-21 13:31:24 +0530

    Print the table with required items in the fisrt

--- wiki_generator.rb
+++ wiki_generator.rb
@@ -10,13 +10,34 @@ def get_type(str)
     return "string"
 end
 
+def print_wiki(hash)
+    print "| #{hash['name']} | #{hash['description']} | #{hash['type']} | #{hash['default']} | #{hash['required']} | \n"
+end
+
+required_arr = []
+non_required_arr = []
+
 parsed.each do |k, v|
     name = k
     description = v["description"]
     type = get_type(v["type"])
-    default = v["default"]
+    default = v["default"] == "" ? "\"\"" : v["default"]
     required = v["default"] ? "No" : "Yes"
-    print "| #{name} | #{description} | #{type} | #{default} | #{required} | \n"
+
+    h = {
+        'name' => name,
+        'description' => description,
+        'type' => type,
+        'default' => default,
+        'required' => required
+    }
+
+    v["default"] ? non_required_arr.push(h) : required_arr.push(h)
 end
+
+print_wiki({'name' => 'Name', 'description' => 'Description', 'type' => 'Type', 'default' => 'Default', 'required' => 'Required'})
+print_wiki({'name' => '------', 'description' => '------', 'type' => '------', 'default' => '------', 'required' => '------'})
+required_arr.map {|h| print_wiki(h)}
+non_required_arr.map {|h| print_wiki(h)}
 
 
